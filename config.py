64886b3e9e7277a988974e7ee8f88d0f208cffaf
StrelitziaHY <45154302+StrelitziaHY@users.noreply.github.com>
2025-03-20 15:32:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = ''
+USERNAME = 'StrelitziaHY'
 TOKEN = ''
 
 # The repository to add this issue to
-REPO_OWNER = ''
-REPO_NAME = ''
+REPO_OWNER = 'StrelitziaHY'
+REPO_NAME = 'ChatDailyPapers'
 
 # Set new submission url of subject
-NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
+NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["", ]
+KEYWORD_LIST = ["gravitational waves", "Moon"]
 
 
 OPENAI_API_KEYS = ['', ]
